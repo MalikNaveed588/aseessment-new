669f7c0693f7b9d45b17bc0e421671c6b41cb380
maliknaveed475 <muhammad.naveed@xstak.com>
2022-07-31 23:42:51 +0500

    fix improvement in design

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,17 +1,89 @@
-import React from "react"
-import "./footer.scss"
-const Footer=()=>{
-    return(
-        <><div className="row footer-style" style={{backgroundColor:"#2b2828"}}>
-            <div className="col-6 d-flex left-footer">
-            <div className="left-text" style={{fontweight:"700",fontSize:"40px",lineHeight:"40px"}}>Get in touch with us for your  service</div>
-            <div className="icons" style={{display:"flex"}}><i className="fa-brands fa-facebook px-1"></i> <i className="fa-brands px-1 fa-twitter"></i>
+import React from "react";
+import "./footer.scss";
+const Footer = () => {
+  return (
+    <>
+      <div
+        className="row footer-style"
+        style={{ backgroundColor: "#2b2828", marginTop: "40px" }}
+      >
+        <div className="col-6 d-flex left-footer">
+          <div
+            className="left-text"
+            style={{ fontweight: "700", fontSize: "40px", lineHeight: "40px" }}
+          >
+            Get in touch with us for your service
+          </div>
+          <div
+            className="icons"
+            style={{
+              display: "flex",
+              textAlign: "left",
+              marginRight: "310px",
+              marginTop: "40px",
+            }}
+          >
+            <i className="fa-brands fa-facebook px-1"></i>{" "}
+            <i className="fa-brands px-1 fa-twitter"></i>
             <i className="fa-brands px-1 fa-instagram"></i>
-            <i className="fa-brands px-1 fa-linkedin"></i></div>
-            </div>
-            <div className="col-6">
-                right
-            </div></div></>
-    )
-}
-export default Footer
+            <i className="fa-brands px-1 fa-linkedin"></i>
+          </div>
+        </div>
+        <div className="col-6 pt-5" style={{ marginTop: "35px" }}>
+          <div
+            style={{
+              fontSize: "14px",
+              fontWeight: "600",
+              lineHeaight: "31.5px",
+              color: "grey",
+            }}
+          >
+            Help line Number
+          </div>
+          <div
+            style={{
+              fontSize: "24px",
+              fontWeight: "600",
+              lineHeaight: "36px",
+              color: "white",
+            }}
+          >
+            1800 265 24 52
+          </div>
+          <div
+            style={{
+              fontSize: "14px",
+              fontWeight: "600",
+              lineHeaight: "31px",
+              color: "grey",
+            }}
+          >
+            Address
+          </div>
+          <div
+            style={{
+              fontSize: "14px",
+              fontWeight: "600",
+              lineHeaight: "31.5px",
+              color: "white",
+            }}
+          >
+            NH 234 Public Square San Francisco 65368
+          </div>
+          <div
+            style={{
+              fontSize: "14px",
+              fontWeight: "600",
+              lineHeaight: "31px",
+              color: "grey",
+            }}
+          >
+            We are open
+          </div>
+          <div>Monday to Friday 9:00 AM to  10:00  AM</div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Footer;
--- src/screens/Blog/index.js
+++ src/screens/Blog/index.js
@@ -3,7 +3,7 @@ import { getBlogs } from "../../API";
 import { useMatch, useNavigate } from "react-router-dom";
 
 import "./index.scss";
-import { Navbar } from "../../components";
+import { Footer, Navbar } from "../../components";
 
 const BlogDetail = () => {
   const [blogData, setBlogData] = useState({});
@@ -26,11 +26,11 @@ const BlogDetail = () => {
       <div className="main-container">
         <div
           style={{
+            display: "flex",
             width: "600px",
             marginTop: "50px",
-            fontSize: "20",
+            fontSize: "20px",
             fontWeight: "700",
-            display: "flex",
             textAlign: "center",
           }}
         >
@@ -50,8 +50,62 @@ const BlogDetail = () => {
           {blogData.Article}
         </div>
 
-        <img src={blogData.Image} width="800px" height="240px" />
+        <img src={blogData.Image} width="1000px" height="400px" />
       </div>
+      <div className="default-dataa">
+        <h1 style={{ marginBottom: "20px" }}>This is a blog post headline</h1>
+        <div
+          style={{
+            fontSize: "14px",
+            display: "flex",
+            marginBottom: "20px",
+            textAlign: "left",
+          }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          dignissim dolor et dui bibendum aliquam. Mauris a quam sit amet dui
+          auctor dictum eget a elit. Pellentesque varius diam risus, ut
+          condimentum lorem volutpat vel. Nam vel orci pharetra eros pulvinar
+          cursus nec quis tellus. Quisque feugiat tortor lectus, pretium
+          interdum justo tincidunt a. Donec at congue lectus. Nulla facilisi.
+          Phasellus consectetur sapien accumsan lectus tincidunt placerat. Etiam
+          ornare nibh vel dui egestas, eu posuere metus convallis. Ut non urna a
+          odio condimentum dictum. Proin egestas erat a orci ultrices, vitae
+          bibendum libero posuere. Quisque laoreet tincidunt justo. Vestibulum
+          congue dictum libero finibus vehicula. Aliquam nisi velit, ultricies
+          eget enim vel, venenatis mollis ante. Maecenas sodales tristique quam.
+          Suspendisse fringilla massa vel dolor ornare rhoncus. Nullam ut orci
+          mattis leo varius laoreet sed mollis dui. Aenean placerat nec enim ut
+          finibus. Maecenas suscipit nibh eu neque egestas, non condimentum mi
+          bibendum. Sed est eros, molestie consectetur auctor non, lobortis quis
+          tortor. Nam cursus imperdiet massa volutpat hendrerit. Sed suscipit
+          ligula iaculis lorem sagittis tincidunt. Etiam pellentesque metus vel
+          enim iaculis aliquam. Mauris at nisi sed elit gravida malesuada.
+        </div>
+        <img src={blogData.Image} width="600px" height="400px" />
+        <h1 style={{ marginTop: "20px" }}>
+          This is a small blog post headline
+        </h1>
+        <div
+          style={{
+            fontSize: "14px",
+            display: "flex",
+
+            textAlign: "left",
+            marginTop: "20px",
+          }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          dignissim dolor et dui bibendum aliquam. Mauris a quam sit amet dui
+          auctor dictum eget a elit. Pellentesque varius diam risus, ut
+          condimentum lorem volutpat vel. Nam vel orci pharetra eros pulvinar
+          cursus nec quis tellus. Quisque feugiat tortor lectus, pretium
+          interdum justo tincidunt a. Donec at congue lectus. Nulla facilisi.
+          Phasellus consectetur sapien accumsan lectus tincidunt placerat. Etiam
+          ornare nibh vel dui egestas, eu posuere metus convallis.
+        </div>
+      </div>
+      <Footer />
     </>
   );
 };
