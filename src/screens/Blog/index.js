fef278206b73692141ced23504da2a98b62e30d1
maliknaveed475 <muhammad.naveed@xstak.com>
2022-07-31 23:02:14 +0500

    create blog information screen

--- src/screens/Blog/index.js
+++ src/screens/Blog/index.js
@@ -3,6 +3,7 @@ import { getBlogs } from "../../API";
 import { useMatch, useNavigate } from "react-router-dom";
 
 import "./index.scss";
+import { Navbar } from "../../components";
 
 const BlogDetail = () => {
   const [blogData, setBlogData] = useState({});
@@ -19,7 +20,40 @@ const BlogDetail = () => {
       });
   }, []);
 
-  return <div>BlogDetail</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="main-container">
+        <div
+          style={{
+            width: "600px",
+            marginTop: "50px",
+            fontSize: "20",
+            fontWeight: "700",
+            display: "flex",
+            textAlign: "center",
+          }}
+        >
+          {blogData.Subtitle}
+        </div>
+        <div
+          style={{
+            width: "600px",
+            marginTop: "20px",
+            fontSize: "10px",
+
+            display: "flex",
+            textAlign: "center",
+            marginBottom: "30px",
+          }}
+        >
+          {blogData.Article}
+        </div>
+
+        <img src={blogData.Image} width="800px" height="240px" />
+      </div>
+    </>
+  );
 };
 
 export default BlogDetail;
